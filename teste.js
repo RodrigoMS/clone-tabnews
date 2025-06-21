648b860d1aec84d88fb1dfaea1f104e9f20ffc62
RodrigoMS <RodrigoMS@users.noreply.github.com>
2025-06-21 02:26:25 +0000

    feat: create and use InternalServerError in /api/v1/status

---
+++ teste.js
@@ -0,0 +1,42 @@
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCOde = 400;
+  }
+}
+
+function saveUser(input) {
+  if (!input) {
+    throw new ReferenceError("É necessário enviar o 'input'.");
+  }
+
+  if (!input.name) {
+    throw new ValidationError("Preencha o seu nome completo.");
+  }
+
+  if (!input.username) {
+    throw new ValidationError("Preencha o seu apelido.");
+  }
+
+  if (!input.age) {
+    throw new ValidationError("Preencha a idade.");
+  }
+
+  user.save(input);
+}
+
+try {
+  saveUser({});
+} catch (error) {
+  if (error instanceof ReferenceError) {
+    throw error;
+  }
+
+  if (error instanceof ValidationError) {
+    console.log(error);
+    return;
+  }
+
+  console.log("Erro desconhecido");
+  console.log(error.stack);
+}
