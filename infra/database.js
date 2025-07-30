8a1c6c02ec80d551bd391d8bfb1ebc29761c5726
RodrigoMS <RodrigoMS@users.noreply.github.com>
2025-07-30 16:39:23 +0000

    feat: use ServiceError in database.js

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -8,9 +9,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
@@ -46,3 +49,5 @@ const database = {
 };
 
 export default database;
+
+//https://github.com/filipedeschamps/clone-tabnews/pull/46
