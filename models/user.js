85373d130b4f666d900a68168a95eb91b40a577a
RodrigoMS <RodrigoMS@users.noreply.github.com>
2025-08-13 12:44:46 +0000

    feat: create 'user.findOneByUsername' method and '/api/v1/users/[username]' endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,37 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  console.log(userFound);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          *
+        FROM 
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado n]ao foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validadeUniqueUserName(userInputValues.username);
@@ -72,6 +104,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
