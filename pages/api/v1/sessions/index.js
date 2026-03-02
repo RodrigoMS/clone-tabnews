2b6fa3553b9a51b55e384652416822a581c3f323
RodrigoMS <RodrigoMS@users.noreply.github.com>
2026-03-02 17:40:33 +0000

    feat: add secure session cookies with HttpOnly

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,4 +1,5 @@
 import { createRouter } from "next-connect";
+import * as cookie from "cookie";
 import controller from "infra/controller.js";
 import authentication from "models/authentication.js";
 import session from "models/session.js";
@@ -18,6 +19,19 @@ async function postHandler(request, response) {
   );
 
   const newSession = await session.create(authenticateUser.id);
+
+  const setCookie = cookie.serialize("session_id", newSession.token, {
+    path: "/",
+    //expires: new Date(newSession.expires_at),
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  // Path=/ - permite que o cookie esteja disponível para para todas
+  // as rotas a partir da raiz.
+  //response.setHeader("Set-Cookie", `session_id=${newSession.token}; Path=/`);
+  response.setHeader("Set-Cookie", setCookie);
 
   return response.status(201).json(newSession);
 }
--- tests/api/v1/sessions/post.test.js
+++ tests/api/v1/sessions/post.test.js
@@ -1,4 +1,5 @@
 import { version as uuidVersion } from "uuid";
+import setCookieParser from "set-cookie-parser";
 import orchestrator from "tests/orchestrator.js";
 import session from "models/session";
 
@@ -140,8 +141,22 @@ describe("POST /api/v1/sessions", () => {
       expect(
         session.EXPIRATION_IN_MILLISECONDS - (expiresAt - createdAt),
       ).toBeLessThan(1000);
+
+      const parsedSetCookie = setCookieParser(response, { map: true });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: responseBody.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: "/",
+        httpOnly: true,
+      });
     });
   });
 });
 
 // Test: npm run test:watch sessions.post
+
+// curl -v -X POST http://localhost:3000/api/v1/users -H "Content-Type: application/json" -d '{"email":"cookie@curso.dev", "username":"cookie", "password":"cookie"}'
+
+// curl -v -X POST http://localhost:3000/api/v1/sessions -H "Content-Type: application/json" -d '{"email":"cookie@curso.dev", "password":"cookie"}'
